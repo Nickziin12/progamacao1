440b315bd514e9c2a745f3f7d5c81652bd2cfa0e
Nickziin12 <alvesnicolas040@gmail.com>
2024-08-05 02:08:35 -0300

    commit2

--- main.cpp
+++ main.cpp
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, soma;
+    int num1, num2, produto;
 
-    
     printf("Digite o primeiro numero inteiro: ");
     scanf("%d", &num1);
 
-    
     printf("Digite o segundo numero inteiro: ");
     scanf("%d", &num2);
 
-    
-    soma = num1 + num2;
+    produto = num1 * num2;
 
-   
-    printf("A soma de %d e %d e %d.\n", num1, num2, soma);
+    printf("O produto de %d e %d e %d.\n", num1, num2, produto);
 
     return 0;
 }
